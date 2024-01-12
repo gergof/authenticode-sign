b1202228c03966c2d500c3bbe368388e8988daaa
Fándly Gergő <gergo@paymoapp.com>
2024-01-12 10:35:37 +0200

    ci: Initialized project

---
+++ test/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+import AuthenticodeSign from '../src/index.js';
+const main = async () => {
+    if (AuthenticodeSign(1, 2) == 3) {
+        console.log('ok');
+    }
+    else {
+        console.log('not ok');
+    }
+};
+main();
