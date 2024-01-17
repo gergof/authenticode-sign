f4c3ee05ed163c541c9c1b79697004d6c301bbab
Fándly Gergő <gergo@paymoapp.com>
2024-01-17 12:01:18 +0200

    chore: Changed SignerObject definition

--- src/SignerObject.ts
+++ src/SignerObject.ts
@@ -2,7 +2,7 @@ import { OID } from './types.js';
 
 export type DigestFn = (data: Iterator<Buffer>) => Promise<Buffer>;
 export type SignFn = (data: Iterator<Buffer>) => Promise<Buffer>;
-export type TimestampFn = (data: Iterator<Buffer>) => Promise<Buffer>;
+export type TimestampFn = (data: Buffer) => Promise<Buffer>;
 
 interface SignerObject {
 	getDigestAlgorithmOid: () => OID;
--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */ 
+/* eslint-disable no-console */
 import fsp from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -7,60 +7,62 @@ import crypto from 'crypto';
 
 const main = async () => {
 	const dir = path.dirname(fileURLToPath(import.meta.url));
-	const workdir = path.join(dir, 'work')
-	await fsp.mkdir(path.join(dir, 'work'), {recursive: true});
+	const workdir = path.join(dir, 'work');
+	await fsp.mkdir(path.join(dir, 'work'), { recursive: true });
 
 	const file = await fsp.readFile(path.join(dir, 'test.exe'));
 
 	const exe = new PEFile(file);
 	console.log('Checksum:', exe.calculateChecksum().toString(16));
 
-	const certDer = await fsp.readFile(path.join(dir, 'signer.cer'))
-	const key = (await fsp.readFile(path.join(dir, 'signer.key'))).toString('utf8')
+	const certDer = await fsp.readFile(path.join(dir, 'signer.cer'));
+	const key = (await fsp.readFile(path.join(dir, 'signer.key'))).toString(
+		'utf8'
+	);
 	const signer = new AuthenticodeSigner({
 		getDigestAlgorithmOid: () => '2.16.840.1.101.3.4.2.1', // sha256
 		getSignatureAlgorithmOid: () => '1.2.840.10045.4.3.2', // ecdsa with sha256
 		getCertificate: () => certDer,
-		digest: async (dataIterator) => {
-			const hash = crypto.createHash('sha256')
+		digest: async dataIterator => {
+			const hash = crypto.createHash('sha256');
 
-			while(true){
+			while (true) {
 				const it = dataIterator.next();
-				if(it.done){
+				if (it.done) {
 					break;
 				}
 
-				await hash.update(it.value)
+				await hash.update(it.value);
 			}
 
-			return hash.digest()
+			return hash.digest();
 		},
-		sign: async (dataIterator) => {
-			const signature = crypto.createSign('sha256')
+		sign: async dataIterator => {
+			const signature = crypto.createSign('sha256');
 
-			while(true){
+			while (true) {
 				const it = dataIterator.next();
-				if(it.done) {
+				if (it.done) {
 					break;
 				}
 
-				await signature.update(it.value)
+				await signature.update(it.value);
 			}
 
-			return signature.sign(key)
+			return signature.sign(key);
 		},
-		timestamp: async (data) => {
-			throw new Error('not implemented')
+		timestamp: async data => {
+			throw new Error('not implemented');
 		}
-	})
+	});
 
 	const result = await signer.sign(exe);
 
-	console.log('Saving result file...')
+	console.log('Saving result file...');
 
 	await fsp.writeFile(path.join(workdir, 'test.signed.exe'), result);
 
-	console.log('Done')
-}
+	console.log('Done');
+};
 
 main();
